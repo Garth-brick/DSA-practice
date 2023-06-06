f283918fc1efb324df65f74b6cb609674441399b
Vagarth <imavagarth@gmail.com>
2023-06-06 15:30:57 +0530

    completed

--- LeetCode/3 - Sliding Window/424_longest_repeating_char_replacement.cpp
+++ LeetCode/3 - Sliding Window/424_longest_repeating_char_replacement.cpp
@@ -4,11 +4,23 @@
 #include <unordered_map>
 using namespace std;
 
+// time O(n)
+// space O(n)
+
 int characterReplacement(string s, int k) {
-    int result=0, max_freq=0;
-    unordered_map<int, int> mappy;
-    for (int i=0; i<s.size(); i++) {
-        
+    int result=0, max_freq=0, l=0, r=0; 
+    // max_freq will track the number of times that the most frequent letter appears. It will kinda keep track of the key with the highest value withing the unordered map.
+    unordered_map<int, int> mappy; // keep a map to track how many of each letter is there in the window
+    while (r < (int)s.size()) {
+        mappy[s[r]]++; // put the current right-pointer's letter into the map
+        max_freq = max(max_freq, mappy[s[r]]); // reset the maximum frequency if needed
+        int curr_length = r-l+1;
+        if (curr_length > k + max_freq) {  // we can only make k replacements for a valid string, so if max_freq+k < curr_length then we won't be to change enough characters to create a valid string.
+            mappy[s[l]]--; // drop the letter at the left pointer and increment the pointer
+            l++;
+        }
+        result = max(result, r-l+1);
+        r++;
     }
     return result;
 }
@@ -22,6 +34,6 @@ int main() {
 }
 
 /*input
-2
-ABAB
+1
+AABABBA
 */
