03da7368e373537a9d03b8107d064c43349fdfef
Vagarth <imvagarth@gmail.com>
2023-06-07 15:21:34 +0530

    first solution, works but exceeds time limit

--- LeetCode/3 - Sliding Window/076_minimum_window_substring.cpp
+++ LeetCode/3 - Sliding Window/076_minimum_window_substring.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 
+// solution is correct but exceeds the time limit
+
+
 string minWindow(string s, string t) {
-    
+    string result="", curr="";
+    unordered_map<char, int> mp1, mp2;
+    for (char c : t) {
+        mp1[c]++;
+    }
+    // mp1 now has the counts of each letter in string t.
+    mp2 = mp1;
+    // mp2 is an exact copy of mp1 now
+    int l=0, r=0;
+    while (r < (int)s.length()) {
+        curr += s[r];
+        if (mp2.count(s[r]) == 1) {
+            // if the letter is present then reduce it's count by 1 in the map
+            mp2[s[r]]--;
+            if (mp2[s[r]] == 0) {
+                // if the count of the letter has gone to zero then remove the key
+                mp2.erase(s[r]);
+
+                if (mp2.empty()) {
+                    // if the map is empty now then check if the new substring is shorter than the result
+                    if (curr.length() < result.length() || result=="") {
+                        result = curr;
+                    }
+                    mp2 = mp1;
+                    l++;
+                    r = l;
+                    curr = "";
+                    continue;
+                }
+           }
+        }
+        r++;
+    }
+    return result;
 }
 
 
@@ -14,3 +52,8 @@ int main() {
     getline(cin, t);
     cout << minWindow(s, t) << endl;
 }
+
+/*input
+a
+aa
+*/
